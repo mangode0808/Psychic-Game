f5eb5ca4ad915e780c44b95546e62a4371805c07
Dulguun Enkhsaikhan <mangode0808@github.com>
2018-03-30 16:33:28 -0500

    added additional styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 
-var alphabetLettersAndNumbers = ["1", "2", "3","4", "5", "6","7","8","9","0","a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphabetLettersAndNumbers = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var wins = 0;
 var losses = 0;
@@ -9,22 +9,22 @@ var guessesSoFar = [];
 var userGuess = null;
 
 var letterAndNumberToBeGuessed = alphabetLettersAndNumbers[Math.floor(Math.random() * alphabetLettersAndNumbers.length)];
-console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " Guesses so far: " + guessesSoFar + " Computer picked: " + letterAndNumberToBeGuessed);
+console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " Guesses so far:   " + guessesSoFar + " Computer picked: " + letterAndNumberToBeGuessed);
 
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
 
-    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();   
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-	if (guessesSoFar.indexOf(userGuess) < 0 && alphabetLettersAndNumbers.indexOf(userGuess) >= 0) {
-		guessesSoFar[guessesSoFar.length]=userGuess;
+    if (guessesSoFar.indexOf(userGuess) < 0 && alphabetLettersAndNumbers.indexOf(userGuess) >= 0) {
+        guessesSoFar[guessesSoFar.length] = userGuess;
 
-		guessesLeft--;
+        guessesLeft--;
     }
-    
-	newFunction(userGuess);
+
+    newFunction(userGuess);
 
 
-	var html = "<p><h1>The Psychic Game</h1></p>" + "<p><h4>Guess what letter I\'m thinking of</h4></p>" + "<p><h4>Wins: " + wins + "</h4></p>" + "<p><h4>Losses: " + losses + "</h4></p>" + "<p><h4>Guesses Left: " + guessesLeft + "</h4></p>" + "Your guesses so far: " + guessesSoFar + "</h4></p>";
+    var html ="<p><h1>The Psychic Game</h1></p>" + "<hr>" + "<p><h2>Guess what letter I'm thinking of</h2></p>" + "<hr>" + "<p><h2>Wins: " + wins + "</h2></p>" + "<p><h2>Losses: " + losses + "</h2></p>" + "<p><h2>Guesses Left: " + guessesLeft + "</h2></p>" + "<p><h2>Your guesses so far: " + guessesSoFar + "</h2></p>" + "<p><iframe src='https://giphy.com/embed/5xaOcLDE64VMF4LqqrK' width='200' height='195' frameBorder='0' class='giphy-embed' allowFullScreen></iframe><p><a href='https://giphy.com/gifs/drake-applause-glasses-5xaOcLDE64VMF4LqqrK'></p>"+  "<p><footer> Copyright &copy; 2018 Dulguun Enkhsaikhan</footer></p>"
 
     document.querySelector("#game").innerHTML = html;
 }
@@ -36,7 +36,7 @@ function newFunction(userGuess) {
         guessesLeft = 9;
         guessesSoFar = [];
         letterAndNumberToBeGuessed = alphabetLettersAndNumbers[Math.floor(Math.random() * alphabetLettersAndNumbers.length)];
-        console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " Guesses so far: " + guessesSoFar + " Computer picked: " + letterAndNumberToBeGuessed);
+        console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " Guesses so far:   " + guessesSoFar + " Computer picked: " + letterAndNumberToBeGuessed);
     }
     if (guessesLeft == 0) {
         losses++;
